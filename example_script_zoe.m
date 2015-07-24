bcd7859a310f2706ac6c4edadb6f868537fba935
zoelooser <zoelooser@gmail.com>
2015-07-24 11:48:19 +0200

    updated plotting

--- example_script_zoe.m
+++ example_script_zoe.m
@@ -8,16 +8,16 @@ pathDLL = ['/Users/zoelooser/Documents/MATLAB/nsMCDLibrary_MacOSX_3.7b/'...
 %% Aglycemia protocol example
 
 % Specify the filenames of the MCD files
-fnMCD_glucose = '2015_06_02_ON01_AglycExp.mcd';
-fnMCD_ramp1 = '2015_06_02_ON01_Stim_Ramp.mcd';
-fnMCD_ramp2 = '2015_06_02_ON01_Stim_Ramp0002.mcd';
-fnMCD_ramp3 = '2015_06_02_ON01_Stim_Ramp0003.mcd';
+fnMCD_glucose = '2015_07_21_N3219_HFS.mcd';
+% fnMCD_ramp1 = '2015_07_14_N3790_Ramp.mcd';
+% fnMCD_ramp2 = '2015_07_14_N3790_Ramp0001.mcd';
+% fnMCD_ramp3 = '2015_07_14_N3790_Ramp0002.mcd';
 
 % Specify the directory of the MCD files
-dirData_glucose = 'F:\Data\Optic Nerve Analysis\Dataset 1';
+dirData_glucose = '/Users/zoelooser/Desktop/collection of recordings/THYATP/N3219';
 
 % Specify which of the 'segment' channels to load
-chToLoad_glucose = 2;
+chToLoad_glucose = 10;
 
 % Call the function to analyse the data sets.  For the first data set, the
 % user will be prompted to select the baseline and area regions
@@ -49,10 +49,10 @@ dataCAP_ramp3 = analyse_CAP(dirData_glucose, fnMCD_ramp3, ...
 %% HFS protocol example
 
 % Specify the filenames of the MCD files
-fnMCD_HFS = '2015_07_17_N2690_HFS0001.mcd';
+fnMCD_HFS = '2015_07_23_N1918_HFS0001.mcd';
 
 % Specify the directory of the MCD files
-dirData_HFS = '/Users/zoelooser/Desktop/stimulation2';
+dirData_HFS = '/Users/zoelooser/Desktop/Weber_Lab/DATA HFS/THYATP/2015_07_23_N1918';
 
 % Specify which of the 'segment' channels to load
 chToLoad_HFS = 10;
@@ -62,3 +62,36 @@ doFit = true;
 dataCAP_HFS = analyse_CAP(dirData_HFS, fnMCD_HFS, ...
     pathDLL, chToLoad_HFS, [], [], doFit);
 
+
+%% HFS protocol example 2
+
+% Specify the filenames of the MCD files
+fnMCD_HFS2 = '2015_07_23_N3220_HFS0001.mcd';
+
+% Specify the directory of the MCD files
+dirData_HFS2 = '/Users/zoelooser/Desktop/Blub';
+
+% Specify which of the 'segment' channels to load
+chToLoad_HFS2 = 10;
+
+% Specify which sweeps to use for normalisation
+idxsNorm = 10:30;
+
+% Call the function to analyse the data sets, this time with fitting
+doFit2 = true;
+[dataCAP_HFS2, dataCAP_HFS2_norm] = analyse_CAP(dirData_HFS2, ...
+    fnMCD_HFS2, pathDLL, chToLoad_HFS2, [], [], doFit2, [], [], idxsNorm);
+
+% Produce a summary plot
+fnFull_plot = fullfile(dirData_HFS2, ...
+    [fnMCD_HFS2(1:end-4) '_summary']);
+plot_summary(dataCAP_HFS2, fnFull_plot)
+
+% Produce a normalised summary plot
+fnFull_plot_norm = fullfile(dirData_HFS2, ...
+    [fnMCD_HFS2(1:end-4) '_summary_norm']);
+plot_summary(dataCAP_HFS2_norm, fnFull_plot_norm)
+
+% Plot the gaussian fit
+nSweep = 50;
+plot_gauss(dataCAP_HFS2, nSweep)
